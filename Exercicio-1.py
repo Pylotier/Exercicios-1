5c8723af4332933dad5c689e320683f068422e08
Pylotier <marcosmario2011@gmail.com>
2026-02-27 13:06:09 -0300

    Update Exercicio-1.py
    O código completo

--- Exercicio-1.py
+++ Exercicio-1.py
@@ -1,5 +1,8 @@
-#Determine as váriaveis
-
-#COMEÇO
-
-#FIM
+#Declaração de variáveis
+lado: float = 0.0;
+res: float = 0.0;
+#Início
+lado = int(input("Digite o lado do quadrado:"))
+res = lado**2;
+print("A área do quadrado é", res, "m²")
+#Fim
